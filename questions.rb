76165bbfe2f2b43189106fea5cb2d845bce10b7c
Peter <peterhsaxton@gmail.com>
2014-02-28 10:25:53 +0000

    start with vowels

--- questions.rb
+++ questions.rb
@@ -1,3 +1,7 @@
 def select_elements_starting_with_a array
   array.select { |item| item[0] == 'a' }
 end
+
+def select_elements_starting_with_vowel array
+  array.select { |item| item =~ /^[aeiou]/ }
+end
