05772cabbfac1a58b8cca5ecd5ad8658cd44cbf0
Peter <peterhsaxton@gmail.com>
2014-02-28 11:07:15 +0000

    copy array until > 5

--- questions.rb
+++ questions.rb
@@ -82,3 +82,12 @@ def average_of_array array
   float = total_of_array(array)/array.length.to_f
   float.round(0)
 end
+
+def get_elements_until_greater_than_five array
+  output = []
+  array.each do |item|
+    break if item > 5
+    output << item
+  end
+  output
+end
